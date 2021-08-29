fc32fad089b03937706d701f019dc4dbf96d35ce
Radu Nicolau <radu.nicolau@gaincredit.com>
2021-08-29 16:37:03 +0100

    2021 update

---
+++ build/precache-manifest.7b06d66029022f310bc0c8ed07f61028.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25dbada160e12a354e72f1c2d0bf1264",
+    "url": "/index.html"
+  },
+  {
+    "revision": "729b2c6e7cb9b0ad765f",
+    "url": "/static/css/main.4898cb0b.chunk.css"
+  },
+  {
+    "revision": "f70d05330805e87c8083",
+    "url": "/static/js/2.58763002.chunk.js"
+  },
+  {
+    "revision": "729b2c6e7cb9b0ad765f",
+    "url": "/static/js/main.559ecb41.chunk.js"
+  },
+  {
+    "revision": "8e689cd4d3827e59490f",
+    "url": "/static/js/runtime-main.e9c2ae90.js"
+  }
+]);
